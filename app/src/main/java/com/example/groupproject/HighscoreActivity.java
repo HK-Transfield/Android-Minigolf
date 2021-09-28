08fb7069c36ab5d5d6fd431ea4cb1eebec9d19e9
Weds <wedswilson@gmail.com>
2021-09-28 22:13:01 +1300

    feat: Add 2 array adapters for the high score list views

--- app/src/main/java/com/example/groupproject/HighscoreActivity.java
+++ app/src/main/java/com/example/groupproject/HighscoreActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class HighscoreActivity extends AppCompatActivity {
 
@@ -21,6 +23,25 @@ public class HighscoreActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
         getWindow().getDecorView().setSystemUiVisibility(uiOptions);
+
+        // string array to retrieve high score rank numbers
+        String[] numList = getResources().getStringArray(R.array.string_array_highscore_numbers);
+        // array adapter to populate the list view
+        ArrayAdapter<String> arrayAdapterRanks = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, numList);
+        // the list view
+        ListView listViewNum = (ListView)findViewById(R.id.highscore_list_numbers);
+        // set adapter
+        listViewNum.setAdapter(arrayAdapterRanks);
+
+        // string array to retrieve high score names
+        String[] nameList = getResources().getStringArray(R.array.string_array_highscore_names);
+        // array adapter to populate the list view
+        ArrayAdapter<String> arrayAdapterNames = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, nameList);
+        // the list view
+        ListView listViewName = (ListView)findViewById(R.id.highscore_list_names);
+        // set adapter
+        listViewName.setAdapter(arrayAdapterNames);
+
     }
 
     // When Play Again button is clicked, go to game screen
