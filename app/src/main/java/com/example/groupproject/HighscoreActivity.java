51eb175c335e87b7a1c0727e9a8b522ebc844415
Weds <wedswilson@gmail.com>
2021-09-28 20:08:39 +1300

    feat: Add fullscreen 'sticky immersive' to High Score screen

--- app/src/main/java/com/example/groupproject/HighscoreActivity.java
+++ app/src/main/java/com/example/groupproject/HighscoreActivity.java
@@ -1,5 +1,6 @@
 package com.example.groupproject;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -12,6 +13,14 @@ public class HighscoreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_highscore);
+
+        // set full screen sticky immersive
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        int uiOptions = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        getWindow().getDecorView().setSystemUiVisibility(uiOptions);
     }
 
     // When Play Again button is clicked, go to game screen
